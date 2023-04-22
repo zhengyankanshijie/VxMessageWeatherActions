b3944f825866d32dac2ba819e0cdc062d59ff28c
zhengyankanshijie <103887109+zhengyankanshijie@users.noreply.github.com>
2023-04-22 09:11:44 +0800

    Update WeatherConfig.java

--- src/main/java/com/pt/vx/config/WeatherConfig.java
+++ src/main/java/com/pt/vx/config/WeatherConfig.java
@@ -9,19 +9,19 @@ public class WeatherConfig {
      * 天气相关的总开关
      * true则为开启，false则为关闭
      */
-    public static boolean OPEN = false;
+    public static boolean OPEN = true;
 
     /**
      * 天气资源的key
      */
-    public static final String weatherSourceKey = "your key";
+    public static final String weatherSourceKey = "5a0b481e0db641dec2bacb05d54d06eb";
 
     /**
      * 天气资源类型
      * 0 为高德地图
      * 1 为和风天气
      */
-    public static Integer weatherSourceType = 1;
+    public static Integer weatherSourceType = 0;
 
     /**
      * 天气类型
